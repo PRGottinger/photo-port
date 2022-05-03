fbc4e043224e03a6a155e1ff0335eef1d77b5832
Paul Gottinger <pgottinger@outlook.com>
2022-05-03 18:55:06 -0500

    about

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from "react";
-import About from "./components/About";
-// import logo from "./logo.svg";
+import Nav from './components/Nav';
+import About from './components/About';
+
 import "./App.css";
 
 function App() {
+
   return (
-    <section className="my-5">
-      <h1 id="about">Who am I?</h1>
-    </section>
+    <div>
+      <Nav />
+      <main>
+          <About />        
+      </main>
+    </div>
   );
 }
 
